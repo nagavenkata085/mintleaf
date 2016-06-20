0bca56a3a9e15bef6807f7226e460ba085431583
Senthil Maruthaiappan <senips@gmail.com>
2016-06-19 23:53:56 -0400

    moving out  dependency and dbs

---
+++ core/src/main/java/org/qamatic/mintleaf/interfaces/db/OracleDbContext.java
@@ -0,0 +1,28 @@
+package org.qamatic.mintleaf.interfaces.db;
+
+import org.qamatic.mintleaf.interfaces.DbContext;
+
+/**
+ * Created by senips on 6/19/16.
+ */
+public interface OracleDbContext extends DbContext {
+
+
+    boolean isPackageExists(String pkgName, boolean igoreValidity);
+
+    boolean isPackageBodyExists(String pkgName, boolean igoreValidity);
+
+
+    boolean isTypeExists(String typeName, boolean igoreValidity);
+
+    boolean isTypeBodyExists(String typeName, boolean igoreValidity);
+
+
+    boolean isTriggerExists(String triggerName, boolean igoreValidity);
+
+    boolean isSynonymExists(String synonymName, boolean igoreValidity);
+
+
+
+
+}
---
+++ dbs/src/test/java/org/qamatic/mintleaf/dbsupportimpls/oracle/ts.java
@@ -0,0 +1,7 @@
+package org.qamatic.mintleaf.dbsupportimpls.oracle;
+
+/**
+ * Created by senips on 6/19/16.
+ */
+public class ts {
+}
